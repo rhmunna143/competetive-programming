9623481451221466f6ac6e848fac7758a93bc9ff
rhmunna143 <rhmunna19@gmail.com>
2024-10-13 13:07:51 +0600

    patterns problems solved

---
+++ Phitron-Practice-Contest/week-02-practice-1-patterns/pattern-3-alphabets-triangle.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    printf("Enter a number as value of N: ");
+    scanf("%d", &n); // Input the value of N
+
+    for (int i = 1; i <= n; i++)
+    {
+        char ch = 'A' + (i - 1); // Determine the character for the current row
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%c ", ch);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+/***
+ * Author:
+ * Md Rabbi Haque Munna
+ * BSc. Engr. in Computer Science, CSE
+ * Rabindra Maitree University, RMU
+ * Batch: 2022-02
+ * Kushtia, Bangladesh
+ *
+ * Email: rhmunna19@gmail.com
+ * WhatsApp: +8801877-255595
+ * LinkedIn: https://www.linkedin.com/in/rhmunna143
+ * GitHub: https://github.com/rhmunna143
+ * Portfolio: https://munna-dev.web.app
+ * Facebook: https://www.facebook.com/Munna.RMU
+ *
+ * Copyright (c) All rights reserved.
+ */
