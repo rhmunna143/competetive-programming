96e3c59d92641475562be59709a808783b70f184
rhmunna143 <rhmunna19@gmail.com>
2024-10-24 20:38:23 +0600

    work save

--- _snipets/basic-c-structure.c
+++ _snipets/basic-c-structure.c
@@ -32,4 +32,32 @@ int main()
  * Facebook: https://www.facebook.com/Munna.RMU
  *
  * Copyright (c) All rights reserved.
+ */#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+    return 0;
+}
+
+/***
+ * Author:
+ * Md Rabbi Haque Munna
+ * BSc. Engr. in Computer Science, CSE
+ * Rabindra Maitree University, RMU
+ * Batch: 2022-02
+ * Kushtia, Bangladesh
+ *
+ * Email: rhmunna19@gmail.com
+ * WhatsApp: +8801877-255595
+ * LinkedIn: https://www.linkedin.com/in/rhmunna143
+ * GitHub: https://github.com/rhmunna143
+ * Portfolio: https://munna-dev.web.app
+ * Facebook: https://www.facebook.com/Munna.RMU
+ *
+ * Copyright (c) All rights reserved.
  */
