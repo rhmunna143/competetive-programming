787b3c8ccb349c7804e8ad0a8f6b0cc054b731e7
rhmunna143 <rhmunna19@gmail.com>
2024-11-29 09:20:03 +0600

    day 9 recursion done

---
+++ Hacker-Rank/30-days-code/day_9_recursion_3/Day_9_Recursion_3.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin >> n;
+
+    cout << factorial(n) << endl;
+    
+    return 0;
+}
